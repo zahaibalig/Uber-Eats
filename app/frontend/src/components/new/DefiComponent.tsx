cd5c1be64c4705bb7a2ce27af639032cb32b816c
zahaibalig <zahaibalig@gmail.com>
2022-12-30 23:10:47 +0100

    component addedd

---
+++ app/frontend/src/components/new/DefiComponent.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DefiComponent = () => {
+  return (
+    <View>
+      <Text>DefiComponent</Text>
+    </View>
+  )
+}
+
+export default DefiComponent
+
+const styles = StyleSheet.create({})
